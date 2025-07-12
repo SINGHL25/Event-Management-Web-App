1672b5d916b87a5ddfee9dde427c37b67b97586d
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-12 21:56:45 +1000

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,13 @@
-
+## 📄 frontend/app.js
+```javascript
+fetch("http://localhost:8000/events")
+  .then(res => res.json())
+  .then(events => {
+    const el = document.getElementById("event-list");
+    events.forEach(evt => {
+      const div = document.createElement("div");
+      div.innerHTML = `<h3>${evt.title}</h3><p>${evt.description}</p>`;
+      el.appendChild(div);
+    });
+  });
+```
